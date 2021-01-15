05c910b7e347117d44ccea0bd6268a92d54f86c1
3EGaming <3EGaming@users.noreply.github.com>
2021-01-15 15:27:03 +0000

    Make pusspuss Login avalible

--- login/login_engine.js
+++ login/login_engine.js
@@ -1,13 +1,25 @@
-var LoginD = function()
+var LoginX = function()
 {
     document.getElementById("PWHL").innerHTML = "";
+    $.getScript("http://3egaming.000webhostapp.com/coco.website.api/pusspuss/login/USERVJ/" + window.UHC + ".js");
     $.getScript("http://3egaming.000webhostapp.com/coco.website.api/pusspuss/login/USERJ/" + window.UHC + ".js");
+    setTimeout(LoginD, 500);
+}
+
+var LoginD = function()
+{
+    if (window.VRFX == 1)
+    {
     if (window.UNHCX == hashCode(document.getElementById("PWT").value))
     {
         console.log("Successfull Login.");
+        window.location.replace("./../?SLI");
     } else {
         console.log("Error");
         document.getElementById("PWHL").innerHTML = "<H3>Username or Password incorrect.</H3>";
+    }
+    } else {
+        document.getElementById("PWHL").innerHTML = "<H3>Your account has not been verified. Please check your email.</H3>";
     }
 }
 
@@ -15,5 +27,5 @@ var LoginA = function()
 {
     window.UHC = hashCode(document.getElementById("UNT").value);
     document.getElementById("PWHL").innerHTML = "<iframe id=\"PWHLF\" height=\"0\" width=\"0\" src=\"http://3egaming.000webhostapp.com/coco.website.api/pusspuss/login/uhl.php?UNH=" + window.UHC + "\"></iframe>";
-    setTimeout(LoginD, 500);
+    setTimeout(LoginX, 500);
 }
