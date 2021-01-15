a25394d065dc5d57ee8a3199d57cb02a9a9d1b52
3EGaming <3EGaming@users.noreply.github.com>
2021-01-15 16:50:22 +0000

    Update pusspuss Login
    logout is now avalible

---
+++ files/js/loginbuttonstate.js
@@ -0,0 +1,4 @@
+if (document.cookie.match(/^(.*;)?\s*username\s*=\s*[^;]+(.*)?$/))
+{
+    document.getElementById("loginlogout").innerHTML = "<img src="https://pusspusswebsite.github.io/files/img/ui/buttons/logoutbutton.png" title="button" style="width:200px;height:100px;">";
+}
